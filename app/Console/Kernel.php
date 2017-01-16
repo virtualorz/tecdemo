54c954c4ddac8b2f71b0da21c3f6b5f470abda21
virtualorz <virtualorz@gmail.com>
2017-01-16 16:00:13 +0800

    使用提示訊息測試版

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,57 +27,31 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule) {
         return;
         $schedule->call(function() {
-            $dtNow = new \DateTime();
-            $dtTomorrow = new \DateTime($dtNow->format('Y/m/d'));
-            $dtTomorrow->add(new \DateInterval('P1D'));
-            
-            $list_course_class = DB::table('course_class')
-                    ->select([
-                        'course_class.id',
-                        'course.code AS course_code',
-                        'course.name_cht AS course_name_cht',
-                        'course.name_eng AS course_name_eng',
-                        DB::raw("DATE_FORMAT(`course_class`.`date_start`, '%Y/%m/%d') AS `date_start`"),
-                        DB::raw("DATE_FORMAT(`course_class`.`date_end`, '%Y/%m/%d') AS `date_end`"),
-                        'course_class_locale.name AS course_class_locale_name',
-                        'course_class.class_way',
-                        'course_class.class_time',
-                        'course.class_time AS course_class_time',
-                    ])
-                    ->leftJoin('course_class_locale', 'course_class.course_class_locale_id', '=', 'course_class_locale.id')
-                    ->leftJoin('course', 'course_class.course_id', '=', 'course.id')
-                    ->where('course_class.enable', 1)
-                    ->where('course_class.status', 1)
-                    ->where('course_class.date_start', $dtTomorrow->format('Y/m/d'))
-                    ->get();
-            
-            foreach ($list_course_class as $k => $v) {
-                $list_apply_item = DB::table('apply_item')
-                        ->select([
-                            'member.email AS member_email',
-                            'member.name_cht AS member_name_cht',
-                        ])
-                        ->leftJoin('member', 'apply_item.member_id', '=', 'member.id')
-                        ->where('apply_item.last_course_class_id', $v['id'])
+            //取得24小時候預約時段名單
+            $tomorrow = date('Y-m-d',strtotime('+1 day',strtotime(date('Y-m-d'))));
+            $notice_list = DB::table('instrument_reservation_data')
+                        ->select('member_data.name as member_name','member_data.email','instrument_reservation_data.reservation_dt','instrument_section.start_time','instrument_data.name')
+                        ->leftJoin('member_data','instrument_reservation_data.member_id','=','member_data.id')
+                        ->leftJoin('instrument_data','instrument_reservation_data.instrument_id','=','instrument_data.id')
+                        ->leftJoin('instrument_section','instrument_reservation_data.reservation_section_id','=','instrument_section.id')
+                        ->whereDate('instrument_reservation_data.reservation_dt','=',$tomorrow)
+                        ->where('instrument_reservation_data.reservation_status','1')
+                        ->where('instrument_data.notice','1')
                         ->get();
-                
-                if (count($list_apply_item) > 0) {
-                    try {
-                        Mail::send('emails.course_start', [
-                            'subject' => trans('email.course_start.subject'),
-                            'data_course_class' => $v,
-                                ], function ($m) use ($list_apply_item) {
-                            foreach ($list_apply_item as $kk => $vv) {
-                                $m->bcc($vv['member_email'], $vv['member_name_cht']);
-                            }
-                            $m->subject(trans('email.course_start.subject'));
-                        });
-                    } catch (\Exception $ex) {
-                        \Log::error('send email "course_start" error: ' . $ex->getMessage());
-                    }
-                }
+            foreach($notice_list as $k=>$v)
+            {
+                //if( (strtotime($v['reservation_dt'].' '.$v['start_time']) - strtotime(date('Y-m-d H:i:s'))) >=82800 && (strtotime($v['reservation_dt'].' '.$v['start_time']) - strtotime(date('Y-m-d H:i:s'))) <= 86400)
+                //{
+                    $dataResult = array('user'=>$v['member_name'],'date'=> $v['reservation_dt'], 'instrument'=>$v['name']);
+                    Mail::send('emails.instrument_use', [
+                                    'dataResult' => $dataResult,
+                                        ], function ($m)use($v) {
+                                    $m->to($v['email'], '');
+                                    $m->subject("系統催繳通知");
+                    });
+                //}
             }
-        })->dailyAt('01:00');
+        })->everyMinute()->withoutOverlapping();
     }
 
 }
---
+++ resources/views/emails/instrument_use.blade.php
@@ -0,0 +1,64 @@
+@extends('emails.layouts.master')
+
+
+@section('content')
+<tr> 
+              <!-- HTML Spacer row -->
+              <td style="font-size: 0; line-height: 0;" height="20"><table width="96%" align="left"  cellpadding="0" cellspacing="0">
+                  <tr>
+                    <td style="font-size: 0; line-height: 0;" height="20">&nbsp;</td>
+                  </tr>
+                </table></td>
+            </tr>
+            <tr> 
+              <!-- HTML Spacer row -->
+              <td style="font-size: 0; line-height: 0;" height="20"><table width="96%" align="left"  cellpadding="0" cellspacing="0">
+                  <tr>
+                    <td style="font-size: 0; line-height: 0;" height="20">&nbsp;</td>
+                  </tr>
+                </table></td>
+            </tr>
+            <tr> 
+              <!-- Introduction area -->
+              <td><table width="96%"  align="left" cellpadding="0" cellspacing="0">
+                  <tr> 
+                    <!-- row container for TITLE/EMAIL THEME -->
+                    <td align="center" style="font-size: 32px; font-weight: 300; line-height: 2.5em; color: #1e6ea2; font-family: sans-serif;">系統訊息通知</td>
+                  </tr>
+                  
+                  <tr>
+                    <td style="font-size: 0; line-height: 0;" height="20"><table width="96%" align="left"  cellpadding="0" cellspacing="0">
+                        <tr> 
+                          <!-- HTML Spacer row -->
+                          <td style="font-size: 0; line-height: 0;" height="20">&nbsp;</td>
+                        </tr>
+                      </table></td>
+                  </tr>
+                  <tr> 
+                    <!-- Row container for Intro/ Description -->
+                    <td align="left" style="font-size: 16px; font-style: normal; font-weight: 100; color: #666; line-height: 1.8; text-align:justify; padding:10px 20px 0px 20px; font-family: sans-serif;"><p>親愛的 {{ $dataResult['user'] }}先生/ 小姐，您好：<br>
+                      您有登記預約{{ $dataReault['date'] }}使用{{ $dateResult['instrument'] }}</p>
+                      <p>提醒您記得出席使用</p>
+                      <p>科技共同空間 敬啟<br>
+                      </p></td>
+                  </tr>
+                </table></td>
+            </tr>
+            <tr> 
+              <!-- HTML Spacer row -->
+            
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+            </tr>
+@endsection
+
+
+
+
