5368bdbd4a24cf4671065bd61ca46ff6a4949baa
virtualorz <virtualorz@gmail.com>
2017-01-16 14:18:25 +0800

    pdf輸出方式修正

--- config/snappy.php
+++ config/snappy.php
@@ -3,13 +3,13 @@
 return array(
     'pdf' => array(
         'enabled' => true,
-        'binary' => '"' . base_path('vendor/vendor/wkhtmltopdf/'.env('OS').'-' . env('OS_BIT') . '/bin/wkhtmltopdf') . '"',
+        'binary' => '"' . base_path('vendor/vendor/wkhtmltopdf/'.env('OS_TYPE').'-' . env('OS_BIT') . '/bin/wkhtmltopdf') . '"',
         'timeout' => false,
         'options' => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => '"' . base_path('vendor/vendor/wkhtmltopdf/'.env('OS').'-' . env('OS_BIT') . '/bin/wkhtmltoimage') . '"',
+        'binary' => '"' . base_path('vendor/vendor/wkhtmltopdf/'.env('OS_TYPE').'-' . env('OS_BIT') . '/bin/wkhtmltoimage') . '"',
         'timeout' => false,
         'options' => array(),
     ),
