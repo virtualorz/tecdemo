696957484bb12e34c8f5d658c9d19af493a0abb2
virtualorz_mac <virtualorz@gmail.com>
2017-02-17 14:47:56 +0800

    儀器排序修正

--- app/Http/Controllers/Official/InstrumentController.php
+++ app/Http/Controllers/Official/InstrumentController.php
@@ -76,7 +76,7 @@ class InstrumentController extends Controller {
                             ->leftJoin('instrument_admin','instrument_admin.instrument_data_id','=','instrument_data.id')
                             ->groupBy('instrument_data.id')
                             ->orderBy('type_name','asc')
-                            ->orderBy('name','asc')
+                            ->orderBy('instrument_id','asc')
                             ->get();
             foreach($instrumentResult as $k=>$v)
             {
