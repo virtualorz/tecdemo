f55330a3803430327bf698e09111fac068e58220
virtualorz_mac <virtualorz@gmail.com>
2017-04-24 17:27:44 +0800

    前台 - 預約通知信增加預約時段顯示

--- app/Console/Commands/ReservationNotice.php
+++ app/Console/Commands/ReservationNotice.php
@@ -35,7 +35,12 @@ class ReservationNotice extends Command
         //取得24小時候預約時段名單
         $tomorrow = date('Y-m-d',strtotime('+1 day',strtotime(date('Y-m-d'))));
         $notice_list = DB::table('instrument_reservation_data')
-                    ->select('member_data.name as member_name','member_data.email','instrument_reservation_data.reservation_dt','instrument_section.start_time','instrument_data.name')
+                    ->select('member_data.name as member_name',
+                            'member_data.email',
+                            'instrument_reservation_data.reservation_dt',
+                            DB::raw('DATE_FORMAT(instrument_section.start_time, "%H:%i") as start_time'),
+                            DB::raw('DATE_FORMAT(instrument_section.end_time, "%H:%i") as end_time'),
+                            'instrument_data.name')
                     ->leftJoin('member_data','instrument_reservation_data.member_id','=','member_data.id')
                     ->leftJoin('instrument_data','instrument_reservation_data.instrument_id','=','instrument_data.id')
                     ->leftJoin('instrument_section','instrument_reservation_data.reservation_section_id','=','instrument_section.id')
@@ -45,9 +50,9 @@ class ReservationNotice extends Command
                     ->get();
         foreach($notice_list as $k=>$v)
         {
-            if( (strtotime($v['reservation_dt'].' '.$v['start_time']) - strtotime(date('Y-m-d H:i:s'))) >=82800 && (strtotime($v['reservation_dt'].' '.$v['start_time']) - strtotime(date('Y-m-d H:i:s'))) <= 86400)
+            if( (strtotime($v['reservation_dt'].' '.$v['start_time'].':00') - strtotime(date('Y-m-d H:i:s'))) >=82800 && (strtotime($v['reservation_dt'].' '.$v['start_time'].':00') - strtotime(date('Y-m-d H:i:s'))) <= 86400)
             {
-                $dataResult = array('user'=>$v['member_name'],'date'=> $v['reservation_dt'], 'instrument'=>$v['name']);
+                $dataResult = array('user'=>$v['member_name'],'date'=> $v['reservation_dt'],'section'=>$v['start_time'].' - '.$v['end_time'], 'instrument'=>$v['name']);
                 Mail::queue('emails.instrument_use', [
                                 'dataResult' => $dataResult,
                                     ], function ($m)use($v) {
